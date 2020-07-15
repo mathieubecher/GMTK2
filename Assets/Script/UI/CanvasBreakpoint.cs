11b8d1d1060d0aacb63b189946e32a8687905f30
Rémi Moreaux <moreaux_rmi@yahoo.fr>
2020-07-15 18:34:34 +0200

    [UI] New Canvas Scale Mode for +750p resolutions

---
+++ Assets/Script/UI/CanvasBreakpoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasBreakpoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Canvas_SmallResolution;
+    public GameObject Canvas_BigResolution;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Screen.height > 750)
+        {
+            Canvas_SmallResolution.SetActive(false);
+            Canvas_BigResolution.SetActive(true);
+        } else
+        {
+            Canvas_SmallResolution.SetActive(true);
+            Canvas_BigResolution.SetActive(false);
+        }
+    }
+}
