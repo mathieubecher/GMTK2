30bde2135abf6f95ff2b97f741daeaa37f3b6bdf
Becher <mbelkhenchir@gmail.com>
2020-07-16 01:06:32 +0200

    DATASS

---
+++ Assets/Script/DatAss.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatAss : MonoBehaviour
+{
+    public AudioClip poisson;
+    public AudioSource chickenSource;
+    public float area = 50;
+    private Camera _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = _camera.WorldToScreenPoint(transform.position);   
+        Debug.Log(Input.mousePosition +" "+ pos);
+        if ((Input.mousePosition - pos).magnitude < area) {
+            if(Input.GetMouseButtonDown(0)) chickenSource.PlayOneShot(poisson);
+        }
+    }
+}
