a5cf062218c60efd59d553e031d5cd692695c60b
Rémi Moreaux <moreaux_rmi@yahoo.fr>
2020-07-16 23:09:06 +0200

    [UI] Upgrade Window Reduction

---
+++ Assets/UI/ButtonUpgrade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonUpgrade : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator ButtonAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonAnimator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ButtonAnimator.SetBool("Show", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ButtonAnimator.SetBool("Show", false);
+    }
+}
